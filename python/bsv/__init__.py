b661c992f8e013881fcea3c6fb411ac15e697ed7
Julie-Fabre <julie.m.fabre@gmail.com>
2024-08-07 13:14:00 +0100

    remove python

--- python/bsv/__init__.py
+++
@@ -1,3 +0,0 @@
-from .connectivity import find_connectivity_experiments, fetch_connectivity_data
-from .plotting import plot_connectivity, plot_connectivity_3d
-#from .utils import some_utility_function  # if you have any utility functions
