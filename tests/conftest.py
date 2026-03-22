b42fa63077fc1e62fd20681ea3d40606ff3a62d8
Julie-Fabre <julie.m.fabre@gmail.com>
2026-03-22 14:04:44 -0400

    fix location and citations

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ import pytest
 # Add parent dir to path so we can import bsv
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-ALLEN_ATLAS_PATH = '/home/julie/Dropbox/Atlas/allenCCF'
-SAVE_LOCATION = '/home/julie/Dropbox/Data/AllenQueries'
+ALLEN_ATLAS_PATH = os.environ.get('ALLEN_ATLAS_PATH', '')
+SAVE_LOCATION = os.environ.get('BSV_SAVE_LOCATION', '')
 
 # Known VISp experiment IDs for testing (small set)
 TEST_EXPERIMENT_IDS = [100141219, 112423392]
